0253a8649f2e950d7db8dd408f9bfbf3ed15adb8
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-05 17:17:39 +0200

    enable advanced features: cors, request size, pipes, interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { urlencoded, json } from 'express';
+
 import { AppModule } from './app.module';
+import { SERVER_PORT } from './app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Create app instance
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  // Add validation pipe
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      enableImplicitConversion: true,
+    }),
+  );
+
+  // Start the server
+  await app.listen(process.env.SERVER_PORT ?? SERVER_PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
