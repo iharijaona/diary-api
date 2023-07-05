825a8ea5e3d02bbf7cb3b12e5624d85982ba0ae2
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-05 17:17:49 +0200

    Default config

---
+++ src/app.config.ts
@@ -0,0 +1,4 @@
+export const SERVER_PORT = 4000;
+export const JWT_SECRET = '477921b3-0392-4a81-a1bf-dbbd9f15fa64';
+export const BCRYPT_SALT_OR_ROUND = 12;
+export const HASHIDS_SECRET = 'cc83469d-a994-4f7f-93b1-bda08bf340f9';
