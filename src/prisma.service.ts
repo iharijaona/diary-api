b6bfaaf05b49c1f420acb184994bb3bdbc70c8fe
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-10 15:53:08 +0200

    Prisma service

---
+++ src/prisma.service.ts
@@ -0,0 +1,37 @@
+import {
+    INestApplication,
+    Injectable,
+    OnModuleDestroy,
+    OnModuleInit,
+  } from '@nestjs/common';
+  import { PrismaClient } from '@prisma/client';
+  
+  @Injectable()
+  export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+  {
+    // constructor() {
+    //   super({ log: ["query"] });
+    // }
+  
+    async onModuleInit() {
+      await this.$connect();
+    }
+  
+    async enableShutdownHooks(app: INestApplication) {
+      this.$on('beforeExit', async () => {
+        await app.close();
+      });
+    }
+  
+    async onModuleDestroy() {
+      await this.$disconnect();
+    }
+  }
+  
+  export type PrismaTrans = Omit<
+    PrismaClient,
+    '$connect' | '$disconnect' | '$on' | '$transaction' | '$use'
+  >;
+  
