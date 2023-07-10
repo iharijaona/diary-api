53fc8ee233fccb16d24ad86af6fef2d110b87b2b
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-10 15:54:32 +0200

    Geolocation module

---
+++ src/geolocation/geolocation.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { CountryModule } from './country/country.module';
+import { LocationModule } from './location/location.module';
+
+@Module({
+  imports: [CountryModule, LocationModule]
+})
+export class GeoLocationModule {}
