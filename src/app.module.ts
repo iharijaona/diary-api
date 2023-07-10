c523e8c3bfc9ae8f4299fa5e674918aee6d3994f
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-10 15:53:35 +0200

    Enable graphql feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path'
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GeoLocationModule } from './geolocation/geolocation.module';
 
 @Module({
-  imports: [],
+  imports: [GeoLocationModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // autoSchemaFile: 'schema.gql',
+      // autoSchemaFile: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      sortSchema: true,
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
