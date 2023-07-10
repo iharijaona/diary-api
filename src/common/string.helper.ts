1097a7e617371d2d8d838496be756ff019af07bc
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-10 15:54:18 +0200

    String helper

---
+++ src/common/string.helper.ts
@@ -0,0 +1,5 @@
+export const toProperCase = (charSequance: string, split=' '): string => {
+   return  charSequance.split(split)
+    .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
+   .join(' ');
+}
