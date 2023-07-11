def4706a3d88c3a1ac1ad2650f2fbb5e207587b7
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-11 13:05:57 +0200

    Location query args

---
+++ src/geolocation/location/dto/query-location.args.ts
@@ -0,0 +1,49 @@
+import { ArgsType, Field, Int, ID, InputType } from '@nestjs/graphql';
+import { Max, Min, IsString, IsOptional, IsObject } from 'class-validator';
+import { PageArgs, EnumSortOrder } from 'src/common/query.metadata';
+import { EnumLocationType } from '../entities/location.entity';
+
+@InputType()
+export class LocationOrderByInput {
+  @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
+  id?: EnumSortOrder;
+
+  @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
+  name?: EnumSortOrder;
+
+  @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
+  code?: EnumSortOrder;
+}
+
+@ArgsType()
+export class LocationQueryArgs extends PageArgs {
+  @Field((type) => Int)
+  @Min(1)
+  @Max(2000)
+  take = 100;
+
+  @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  filter?: string;
+
+  @Field({ nullable: true })
+  @IsObject()
+  @IsOptional()
+  orderBy?: LocationOrderByInput;
+
+  @Field((type) => EnumLocationType, { nullable: true })
+  @IsOptional()
+  type?: EnumLocationType;
+
+  @Field((type) => ID, { nullable: true })
+  @IsOptional()
+  parentId?: string;
+
+  @Field((type) => ID, { nullable: true })
+  @IsOptional()
+  countryId?: string;
+}
