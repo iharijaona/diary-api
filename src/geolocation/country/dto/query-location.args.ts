896d53bbf33dcb485b5b070ed8eb39bdee49674a
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-11 10:11:58 +0200

    Fix optional attributes

--- src/geolocation/country/dto/query-location.args.ts
+++ src/geolocation/country/dto/query-location.args.ts
@@ -1,19 +1,23 @@
 import { ArgsType, Field, Int, InputType } from '@nestjs/graphql';
-import { Max, Min } from 'class-validator';
+import { Max, Min, IsString, IsOptional, IsObject } from 'class-validator';
 import { PageArgs, EnumSortOrder } from 'src/common/query.metadata';
 
 @InputType()
 export class CountryOrderByInput {
   @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
   name?: EnumSortOrder
 
   @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
   nationality?: EnumSortOrder
 
   @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
   alpha2Code?: EnumSortOrder
 
   @Field((type) => EnumSortOrder, { nullable: true })
+  @IsOptional()
   alpha3Code?: EnumSortOrder
 }
 
@@ -25,9 +29,13 @@ export class CountryQueryArgs extends PageArgs {
   take = 100;
 
   @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
   filter?: string;
 
   @Field({ nullable: true })
+  @IsObject()
+  @IsOptional()
   orderBy?: CountryOrderByInput
 }
 
