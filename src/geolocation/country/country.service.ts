277bfa5a244f680f18018a1eebed2e4adb71511f
Harijaona Ravelondrina <hravelondrina@gmail.com>
2023-07-11 10:10:58 +0200

    Enable filter in countries query

--- src/geolocation/country/country.service.ts
+++ src/geolocation/country/country.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-
+import { Prisma } from '@prisma/client';
 import { decodeId, encodeId } from 'src/common/hashids.helper';
 import { PrismaService } from 'src/prisma.service';
 import { Country, PaginatedCountry } from './entities/country.entity';
@@ -11,16 +11,36 @@ export class CountryService {
   constructor(private prisma: PrismaService) {}
 
   async findAllCountry(args: CountryQueryArgs): Promise<PaginatedCountry> {
-    const totalItemCount = await this.prisma.country.count();
+    // Filter
+    const whereFilter: Prisma.CountryWhereInput  = args.filter
+    ? {
+      OR: [
+        { name: { contains: args.filter, mode: "insensitive" } },
+        { name_fr: { contains: args.filter, mode: "insensitive" } },
+        { name_en: { contains: args.filter, mode: "insensitive" } },
+        { nationality: { contains: args.filter, mode: "insensitive" } },
+        { nationality_en: { contains: args.filter, mode: "insensitive" } },
+      ],
+    }
+    : {}
+
+    console.log("args.orderBy => ", args.orderBy);
+    console.log(JSON.stringify(args.orderBy));
+
+    // Compute pagination metadata
+    const totalItemCount = await this.prisma.country.count({ where : whereFilter });
     const totalPageCount = Math.ceil(totalItemCount / (args.take > 0 ? args.take : 1)) 
     const currentPage = Math.floor(args.skip / (args.take > 0 ? args.take : 1)) + 1
+    const filteredCountries = await Promise.all(
+      (await this.prisma.country.findMany({ skip: args.skip, take: args.take, where : whereFilter, orderBy: args.orderBy  as Prisma.Enumerable<Prisma.CountryOrderByWithRelationInput> })).map((item) => ({
+        ...item,
+        id: encodeId(item.id)
+      })),
+    );
+
+    // Return the results
     return {
-      nodes: await Promise.all(
-        (await this.prisma.country.findMany({ skip: args.skip, take: args.take })).map((item) => ({
-          ...item,
-          id: encodeId(item.id)
-        })),
-      ),
+      nodes: filteredCountries,
       pageSize: args.take,
       totalPageCount,
       totalItemCount,
